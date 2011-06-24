b8ddf0ed455da8a3decdf5d348742c93f40b94ce
dekosuke <deco.elb@gmail.com>
2011-06-25 04:56:37 +0900

    files added

---
+++ src/cx/ath/dekosuke/chikuwadoid/ImageCache.java
@@ -0,0 +1,102 @@
+package cx.ath.dekosuke.chikuwadoid;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.util.Log;
+import java.util.Iterator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.net.URL;
+
+public class ImageCache {
+
+	public static Bitmap getImage(String url) {
+		String urlHash = FutabaCrypt.createDigest(url);
+		// 本当はキーを画像名ではなくスレッド名含むURLにすべき
+		try {
+			if (SDCard.cacheExist(urlHash)) {
+				return SDCard.loadBitmapCache(urlHash);
+			}
+		} catch (Exception e) {
+			FLog.d("message", e);
+		}
+		return null;
+	}
+
+	// Bitmap→バイトデータ
+	private static byte[] bmp2data(Bitmap src, Bitmap.CompressFormat format,
+			int quality) {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		src.compress(format, quality, os);
+		return os.toByteArray();
+	}
+
+	public static boolean setImage(String url) {
+		String urlHash = FutabaCrypt.createDigest(url);
+		try {
+			SDCard.saveFromURL(urlHash, new URL(url), true);
+			return true;
+		} catch (Exception e) {
+			FLog.d("message", e);
+		}
+		return false;
+	}
+
+	public static boolean setImageFromBitmap(String url, Bitmap bmp) {
+		String urlHash = FutabaCrypt.createDigest(url);
+		byte[] bytes = bmp2data(bmp, Bitmap.CompressFormat.PNG, 100);
+		try {
+			// SDCard.saveFromURL(urlHash, new URL(url), true);
+			SDCard.saveBin(urlHash, bytes, true);
+			return true;
+		} catch (Exception e) {
+			FLog.d("message", e);
+		}
+		return false;
+	}
+
+	public static File saveImage(String url) {
+		String urlHash = FutabaCrypt.createDigest(url);
+		File file = new File(url);
+		if (SDCard.cacheExist(urlHash)) {
+			try {
+				return SDCard.copyCacheToFile(urlHash, file.getName());
+			} catch (Exception e) {
+				FLog.d("message", e);
+			}
+		} else {
+			// キャッシュがない(通常あまりないはずだが・・)
+			// 再読み込みしてください、みたいな例外を投げましょう
+			/*
+			 * setImage(url); //再帰 saveImage(url);
+			 */
+		}
+		return null;
+	}
+	
+	public static File saveImageToThread(String url, String threadName) {
+		String urlHash = FutabaCrypt.createDigest(url);
+		File file = new File(url);
+		if (SDCard.cacheExist(urlHash)) {
+			try {
+				return SDCard.copyCacheToThreadFile(urlHash, file.getName(), threadName);
+			} catch (Exception e) {
+				FLog.d("message", e);
+			}
+		} else {
+			// キャッシュがない(通常あまりないはずだが・・)
+			// 再読み込みしてください、みたいな例外を投げましょう
+			/*
+			 * setImage(url); //再帰 saveImage(url);
+			 */
+		}
+		return null;
+	}
+
+	public static void GC() {
+		// currently do nothing
+	}
+}
---
+++ src/cx/ath/dekosuke/chikuwadoid/ImageResizer.java
@@ -0,0 +1,53 @@
+package cx.ath.dekosuke.chikuwadoid;
+
+import android.graphics.Bitmap;
+import java.lang.Math;
+import android.util.Log;
+import android.graphics.Matrix;
+
+public class ImageResizer {
+	public static Bitmap ResizeWideToSquare(Bitmap bmp) {
+		int width = bmp.getWidth();
+		int height = bmp.getHeight();
+		int size = Math.max(width, height);
+		// Matrix matrix = new Matrix();
+		int pixels[] = new int[width * height];
+		bmp.getPixels(pixels, 0, width, 0, 0, width, height);
+
+		Bitmap ret = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+		if (width > height) {
+			ret.setPixels(pixels, 0, width, 0, (width - height) / 2, width,
+					height);
+		} else {
+			ret.setPixels(pixels, 0, width, (height - width) / 2, 0, width,
+					height);
+		}
+		return ret;
+	}
+
+	public static Bitmap ResizeCutToSquare(Bitmap bmp) {
+		int width = bmp.getWidth();
+		int height = bmp.getHeight();
+		int size = Math.min(width, height);
+		// Matrix matrix = new Matrix();
+		int pixels[] = new int[size * size];
+		int stride = Math.max(width, height);
+		bmp.getPixels(pixels, 0, size, Math.max(0, (width - height) / 2),
+				Math.max(0, (height - width) / 2), size, size);
+
+		Bitmap ret = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+		ret.setPixels(pixels, 0, size, 0, 0, size, size);
+		/*
+		 * if (width > height) { ret.setPixels(pixels, 0, width, 0, (width -
+		 * height) / 2, width, height); } else { ret.setPixels(pixels, 0, width,
+		 * (height - width) / 2, 0, width, height); }
+		 */
+		return ret;
+	}
+
+	public static Bitmap ResizeCenter(Bitmap bmp, int size) {
+		Bitmap temp = ResizeCutToSquare(bmp);
+		return Bitmap.createScaledBitmap(temp, size, size, true);
+	}
+
+}
